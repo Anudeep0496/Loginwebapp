a609a51b85a7da199eabb2901621b394b9381f3f
Anudeep0496 <anudeep0496m@gmail.com>
2025-05-26 01:02:05 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,60 @@
 pipeline {
-    agent any  // Runs on your local Windows machine
-
+    agent any
     environment {
-        AWS_REGION = 'us-east-1'
-        ECR_REGISTRY = '105306707871.dkr.ecr.us-east-1.amazonaws.com'
+        AWS_REGION     = 'us-east-1'
+        AWS_ACCOUNT_ID = '105306707871'
+        ECR_REPO       = 'loginwebapp-ecr'
+        ECR_REGISTRY   = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+        IMAGE_TAG      = "${BUILD_NUMBER}"
+        TERRAFORM_DIR  = './terraform'
     }
 
     stages {
-            stage('Check Docker') {
-      steps {
-        bat 'docker --version'
-      }
-    }
+        stage('Checkout Code') {
+            steps {
+                checkout scm
+            }
+        }
+
         stage('Login to AWS ECR') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'aws-credentials', 
-                                                usernameVariable: 'AWS_ACCESS_KEY_ID', 
-                                                passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                    bat """
-                    aws configure set aws_access_key_id %AWS_ACCESS_KEY_ID%
-                    aws configure set aws_secret_access_key %AWS_SECRET_ACCESS_KEY%
-                    aws configure set region %AWS_REGION%
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'aws-credentials',
+                        usernameVariable: 'AWS_ACCESS_KEY_ID',
+                        passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                    )
+                ]) {
+                    sh '''
+                        aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+                        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+                        aws configure set region $AWS_REGION
 
-                    aws ecr get-login-password --region %AWS_REGION% | docker login --username AWS --password-stdin %ECR_REGISTRY%
-                    """
+                        aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
+                    '''
                 }
             }
         }
-        // Add other stages here (build docker image, push etc.)
+
+        stage('Build and Push Docker Image') {
+            steps {
+                sh '''
+                    docker build -t $ECR_REPO:$IMAGE_TAG .
+                    docker tag $ECR_REPO:$IMAGE_TAG $ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG
+                    docker push $ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG
+                '''
+            }
+        }
+
+        stage('Terraform Init and Apply') {
+            steps {
+                dir("${TERRAFORM_DIR}") {
+                    sh '''
+                        terraform init
+                        terraform apply -auto-approve -var="image_tag=$IMAGE_TAG"
+                    '''
+                }
+            }
+        }
     }
 }
