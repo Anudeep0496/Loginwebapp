bca3994424b5bdc83fa0cff8907e010954f0717d
Anudeep0496 <anudeep0496m@gmail.com>
2025-05-26 00:37:12 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,6 +5,7 @@ pipeline {
         AWS_REGION = 'us-east-1'
         ECR_REPO = 'loginwebapp-ecr'
         AWS_ACCOUNT_ID = '105306707871'
+        IMAGE_TAG = '' // Will be set in a later stage
         TERRAFORM_DIR = './terraform'
     }
 
@@ -12,41 +13,42 @@ pipeline {
         stage('Set IMAGE_TAG') {
             steps {
                 script {
-                    env.IMAGE_TAG = env.BUILD_NUMBER
+                    env.IMAGE_TAG = "${env.BUILD_NUMBER}"
                 }
             }
         }
 
-        stage('Checkout') {
+        stage('Checkout Code') {
             steps {
                 checkout scm
             }
         }
 
-        stage('Login to ECR') {
+        stage('Login to Amazon ECR') {
             steps {
                 sh """
-                aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+                    aws ecr get-login-password --region ${AWS_REGION} | \
+                    docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
                 """
             }
         }
 
-        stage('Build & Push Docker Image') {
+        stage('Build and Push Docker Image') {
             steps {
                 sh """
-                docker build -t $ECR_REPO:$IMAGE_TAG .
-                docker tag $ECR_REPO:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG
-                docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG
+                    docker build -t ${ECR_REPO}:${IMAGE_TAG} .
+                    docker tag ${ECR_REPO}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
+                    docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
                 """
             }
         }
 
-        stage('Terraform Init & Apply') {
+        stage('Terraform Init and Apply') {
             steps {
                 dir("${TERRAFORM_DIR}") {
                     sh """
-                    terraform init
-                    terraform apply -auto-approve -var="image_tag=$IMAGE_TAG"
+                        terraform init
+                        terraform apply -auto-approve -var="image_tag=${IMAGE_TAG}"
                     """
                 }
             }
