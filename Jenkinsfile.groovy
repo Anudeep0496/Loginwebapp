3301c6486a788a181e167f345d1f9d66cd430259
Anudeep0496 <anudeep0496m@gmail.com>
2025-05-26 01:04:20 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,24 +17,24 @@ pipeline {
         }
 
         stage('Login to AWS ECR') {
-            steps {
-                withCredentials([
-                    usernamePassword(
-                        credentialsId: 'aws-credentials',
-                        usernameVariable: 'AWS_ACCESS_KEY_ID',
-                        passwordVariable: 'AWS_SECRET_ACCESS_KEY'
-                    )
-                ]) {
-                    sh '''
-                        aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-                        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-                        aws configure set region $AWS_REGION
+    steps {
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'aws-credentials',
+                usernameVariable: 'AWS_ACCESS_KEY_ID',
+                passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+            )
+        ]) {
+            bat """
+                aws configure set aws_access_key_id %AWS_ACCESS_KEY_ID%
+                aws configure set aws_secret_access_key %AWS_SECRET_ACCESS_KEY%
+                aws configure set region %AWS_REGION%
 
-                        aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
-                    '''
-                }
-            }
+                aws ecr get-login-password --region %AWS_REGION% | docker login --username AWS --password-stdin %ECR_REGISTRY%
+            """
         }
+    }
+}
 
         stage('Build and Push Docker Image') {
             steps {
